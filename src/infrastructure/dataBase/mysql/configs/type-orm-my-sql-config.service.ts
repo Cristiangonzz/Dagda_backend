0c96b9cb753a2a2d474a11a7af90b994f094879b
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-06-23 12:05:01 -0300

    true a la cnfig de la base de datos

--- src/infrastructure/dataBase/mysql/configs/type-orm-my-sql-config.service.ts
+++ src/infrastructure/dataBase/mysql/configs/type-orm-my-sql-config.service.ts
@@ -39,7 +39,7 @@ export class TypeOrmMySqlConfigService implements TypeOrmOptionsFactory {
         NodeMailerMySqlEntity,
         imagenMySqlEntity,
       ],
-      synchronize: false,
+      synchronize: true,
     };
   }
 }
