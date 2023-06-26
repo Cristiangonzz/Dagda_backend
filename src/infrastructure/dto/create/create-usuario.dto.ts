97200456a24187793d16450cdce9866f81e192e0
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-06-25 23:32:57 -0300

    algo

--- src/infrastructure/dto/create/create-usuario.dto.ts
+++ src/infrastructure/dto/create/create-usuario.dto.ts
@@ -4,44 +4,30 @@ import { ApiProperty } from "@nestjs/swagger";
 export class RegistrarUsuarioDto {
 
     @ApiProperty()
-    @IsString()
     primer_nombre: string;
     
     @ApiProperty()
-    @IsString()
     segundo_nombre?: string;
 
     @ApiProperty()
-    @IsString()
     primer_apellido: string;
 
     @ApiProperty()
-    @IsString()
     segundo_apellido?: string;
 
     @ApiProperty()
-    @IsNumber()
-    @IsPositive()
     telefono: number;
 
-    
     @ApiProperty()
-    @IsString()
     foto?: string;
 
     @ApiProperty()
-    @IsString()
     usuario: string;
 
     @ApiProperty()
-    @IsString()
     clave: string;
-
-    
+ 
     @ApiProperty()
-    @IsString()
     @IsEmail()
     email: string;
-
-    
 }
